8c9f9d978c367ec66604429bb29243f47ee4c5ca
Caio Tadashi <ctdsh@icloud.com>
2021-06-24 18:29:28 -0300

    changed modal buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,18 +2,30 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => { 
-    button.addEventListener('click', event => {
-        modal.open()
-    })
+    button.addEventListener('click', handleClick)
 })
 
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
-    button.addEventListener('click', event => {
-        modal.open()
-    })
+    button.addEventListener('click', (event) => handleClick(event,false))
 })
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? 'Marcar como lida' : 'Excluir'
+
+    modalTitle.innerHTML = `${text} esta pergunta?`
+    modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+    modal.open()
+}
