927610e0e3c60d8ca1c18b6500ec88a46b6820ff
Caio Tadashi <ctdsh@icloud.com>
2021-06-25 10:58:48 -0300

    added modal actions and routes

--- src/route.js
+++ src/route.js
@@ -6,4 +6,6 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req,res) => res.render('room'))
 route.get('/create-pass', (req,res) => res.render('create-pass'))
 
+// route.get('/room/:room/:question/:action')
+
 module.exports = route
