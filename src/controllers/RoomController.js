ccbe13eb1081c6b79a940793099f215cf3e4cbb5
Caio Tadashi <ctdsh@icloud.com>
2021-06-27 12:15:36 -0300

    fixed passwords with lettters

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -23,7 +23,7 @@ module.exports = {
                     pass
                 ) VALUES (
                     ${parseInt(roomId)},
-                    ${pass} 
+                    '${pass}' 
                 )`)
             }
 
